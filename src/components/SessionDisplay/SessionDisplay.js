7302affda748bfb404b986ec474903f04ec9b99d
Agnaldo Cardoso Junior <acjr.developer@gmail.com>
2020-05-01 20:45:03 -0300

    redefined components calls

--- src/components/SessionDisplay/SessionDisplay.js
+++ src/components/SessionDisplay/SessionDisplay.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import useCountdownTimer from '../../hooks/useCountdownTimer';
+import Clock from '../Clock';
 
 function SessionDisplay() {
-  const {
-    minutes,
-    seconds,
-    isCountingdown,
-    startCountdown,
-  } = useCountdownTimer({
-    mm: 1,
-    ss: 10,
-  });
-
   return (
     <div>
       <h2>SessionDisplay</h2>
       <div>display</div>
-      <div>is countingdown: {isCountingdown}</div>
-      <div>
-        {minutes}:{seconds}
-      </div>
-      <div onClick={startCountdown}>start countdown</div>
+      <Clock minutes={1} seconds={5} />
     </div>
   );
 }
